d84663841bd81b6e2946733e83a0cac6cf83a31e
Travis Stevenson <thetechtravis@outlook.com>
2020-10-26 15:42:52 -0500

    Created EventListener to listen for gameTicketPurchased event. Responds by adding a new div to the DOM.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 // Import and invoke the ticket booth component function
 
 import { FoodTicketHolders } from "./food/FoodTicketHolders.js"
+import { GameTicketHolders } from "./games/GameTicketHolders.js"
 import { RideTicketHolders } from "./rides/RideTicketHolders.js"
 import { TicketBooth } from "./TicketBooth.js"
 
 TicketBooth()
 RideTicketHolders()
 FoodTicketHolders()
+GameTicketHolders()
