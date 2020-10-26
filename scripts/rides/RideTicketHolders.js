54659ea350cb45915e2ce5e00e76d5aef99682c4
Travis Stevenson <thetechtravis@outlook.com>
2020-10-26 14:12:26 -0500

    Created EventListener to listen for rideTicketPurchased event. Responds by adding a new div to the DOM.

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -1,8 +1,15 @@
 const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
+// Listen for rideEvent -> Add a div with classes "person" and "rider"
 export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
+    
+    eventHub.addEventListener("rideTicketPurchased", rideTicketClicked => {
+        console.log("You are responding to rideEvent dispatch!")
+
+        contentTarget.innerHTML +=
+        `
+        <div class="person rider"></div>
+        `
     })
 }
-
