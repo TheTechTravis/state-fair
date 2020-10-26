f7484243df12811581025811212ef6169c3c88a4
Travis Stevenson <thetechtravis@outlook.com>
2020-10-26 12:13:17 -0500

    Added EventListener to TicketBooth.js that dispatches when a ClickEvent happens on the Ride Ticket button.

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,12 +1,20 @@
-const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
+const contentTarget = document.querySelector(".entry")
 
-eventHub.addEventListener()
+// Only act if the click is on an element with the id value of "rideTicket"
+eventHub.addEventListener("click", ClickEvent => {
+
+    if (ClickEvent.target.id === "rideTicket") {
+        const rideEvent = new CustomEvent("rideTicketPurchased")
+        console.log("You clicked on the button!")
+        dispatchEvent(rideEvent)
+    }
+})
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
-            Add button here
+            <button id="rideTicket"> Ride Ticket </button>
         </div>
     `
 }
